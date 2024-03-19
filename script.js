6ff374d995f304842b922759c95961f166e44e71
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2024-03-19 10:18:44 -0300

    innerHtml added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const bannerImg = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
 
 focoBtn.addEventListener('click', () => {
     changedContext('foco')
@@ -16,7 +17,26 @@ longoBtn.addEventListener('click', () => {
     changedContext('descanso-longo')
 })
 
-function changedContext(context){
+function changedContext(context) {
     html.setAttribute('data-contexto', context)
-    bannerImg.setAttribute('src',`/imagens/${context}.png`)
+    bannerImg.setAttribute('src', `/imagens/${context}.png`)
+    switch (context) {
+        case 'foco':
+            title.innerHTML = `  Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+
+        case 'descanso-curto':
+            title.innerHTML = ` Que tal dar uma respirada,<br>
+            <strong class="app__title-strong">Faça uma pausa curta</strong>`
+            break;
+
+        case 'descanso-longo':
+            title.innerHTML = ` Hora de voltar à superfície,<br>
+            <strong class="app__title-strong">Faça uma pausa longa</strong>`
+            break;
+
+        default:
+            break;
+    }
 }
