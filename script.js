550427bc54bb886a9087372e01ef7f75174d0e63
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2024-03-19 11:11:42 -0300

    audio added

--- script.js
+++ script.js
@@ -4,20 +4,41 @@ const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const bannerImg = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
+const allBtns = document.querySelectorAll('.app__card-button');
+const musicInput = document.getElementById('alternar-musica');
+const music = new Audio('sons/luna-rise-part-one.mp3');
+music.loop = true;
+
+musicInput.addEventListener('change', () =>{
+    if(music.paused){
+        music.play();
+    }else{
+        music.paused;
+    }
+}
+
+)
 
 focoBtn.addEventListener('click', () => {
     changedContext('foco')
+    focoBtn.classList.add('active');
 });
 
 curtoBtn.addEventListener('click', () => {
     changedContext('descanso-curto')
+    curtoBtn.classList.add('active')
 })
 
 longoBtn.addEventListener('click', () => {
     changedContext('descanso-longo')
+    longoBtn.classList.add('active')
 })
 
 function changedContext(context) {
+    allBtns.forEach(function (context) {
+        context.classList.remove('active')
+    })
+
     html.setAttribute('data-contexto', context)
     bannerImg.setAttribute('src', `/imagens/${context}.png`)
     switch (context) {
