0f43365af3dc5045f245a0c7943b8c96c6e69b10
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2024-03-19 09:53:06 -0300

    Images set

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const html = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
+const bannerImg = document.querySelector('.app__image');
 
 focoBtn.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    bannerImg.setAttribute('src', '/imagens/foco.png')
 });
 
 curtoBtn.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    bannerImg.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoBtn.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+    bannerImg.setAttribute('src', '/imagens/descanso-longo.png')
 })
