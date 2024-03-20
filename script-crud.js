635d60abfcaac8ea888d8f498c9504f7fd98b752
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2024-03-20 11:45:10 -0300

    text form and local storage

--- script-crud.js
+++ script-crud.js
@@ -1,12 +1,21 @@
 //encontrar o botão para adicionar nova tarefa
 const addNewTaskBtn = document.querySelector('.app__button--add-task');
 const formAddTask = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
 
-
-
+const taskList = [];
 
 
 addNewTaskBtn.addEventListener('click', () => {
     formAddTask.classList.toggle('hidden');
 })
 
+formAddTask.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const task = {
+        description : textArea.value
+    }
+    taskList.push(task)
+    localStorage.setItem('task', taskList);
+})
