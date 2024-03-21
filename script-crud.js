f725e48b98a5d011e41c7e143dc5fbcfbcb3cf7e
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2024-03-21 10:45:53 -0300

    task list listed

--- script-crud.js
+++ script-crud.js
@@ -2,9 +2,39 @@
 const addNewTaskBtn = document.querySelector('.app__button--add-task');
 const formAddTask = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
+const ulTask = document.querySelector('.app__section-task-list');
 
-const taskList = [];
+const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
 
+function createTaskElement(task) {
+    //Criei uma li e adicionei uma classe a ela.
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+</svg> 
+`
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = task.description;
+    paragraph.classList.add('app__section-task-list-item-description');
+
+    const button = document.createElement('button');
+    button.classList.add('app_button-edit');
+    const imgBtn = document.createElement('img');
+    imgBtn.setAttribute('src', '/imagens/edit.png');
+    button.append(imgBtn);
+
+    li.append(svg);
+    li.append(paragraph);
+    li.append(button);
+
+    return li;
+}
 
 addNewTaskBtn.addEventListener('click', () => {
     formAddTask.classList.toggle('hidden');
@@ -16,33 +46,17 @@ formAddTask.addEventListener('submit', (event) => {
     const task = {
         description: textArea.value
     }
+     
     taskList.push(task)
-    localStorage.setItem('task', JSON.stringify(taskList)); //o JSON. stringify transforma o conteudo da taskList numa string
+    const taskElement = createTaskElement(task);
+    ulTask.append(taskElement);
+    localStorage.setItem('tasks', JSON.stringify(taskList)); //o JSON. stringify transforma o conteudo da taskList numa string
+    textArea.value = '';
+    formAddTask.classList.add('hidden');
 })
 
-function createTaskElement(task) {
-    //Criei uma li e adicionei uma classe a ela.
-    const li = document.createElement('li');
-    li.classList.add('.app__section-task-list-item');
 
-    const svg = document.createElement('svg');
-    svg.innerHTML = `<svg>
-    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-    </svg>
-</svg> `
-
-    const paragraph = document.createElement('p');
-    paragraph.textContent = task.description;
-
-    const button = document.createElement('button');
-    const imgBtn = document.createElement('img');
-    imgBtn.setAttribute('src', '/imagens/edit.png');
-    button.append(imgBtn);
-
-    li.append(svg);
-    li.append(paragraph);
-    li.append(button);
-
-}
+taskList.forEach(task => {
+    const taskElement = createTaskElement(task);
+    ulTask.append(taskElement);
+});
